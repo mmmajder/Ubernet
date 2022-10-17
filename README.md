b032f7d5580a7714701a0eea624079c79e9d1c8a
mmmajder <ajder.milan2000@gmail.com>
2022-10-17 20:51:35 +0200

    initialised spring project and created postgre db

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Uber
+
+Aleksa Stanivuk SW29/2019
+Anđela Mišković SW33/2019
+Milan Ajder SW31/2019
---
+++ ubernet-back/src/main/java/com/example/ubernet/UbernetApplication.java
@@ -0,0 +1,13 @@
+package com.example.ubernet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbernetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbernetApplication.class, args);
+    }
+
+}
