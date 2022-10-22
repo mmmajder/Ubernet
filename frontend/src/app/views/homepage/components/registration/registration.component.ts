7c2e32018f5adcead68847a2b5107b402eee7a0a
Anđela <andjelamiskovic2000@gmail.com>
2022-10-22 21:10:06 +0200

    [US-2]::registration form created

---
+++ frontend/src/app/views/homepage/components/registration/registration.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormControl, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent implements OnInit {
+  email: string = "";
+  phoneNumber: string = "";
+  password: string = "";
+  password2: string = "";
+  name: string = "";
+  lastName: string = "";
+  city: string = "";
+
+  hide: boolean = true;
+  hide2: boolean = true;
+
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  phoneFormControl = new FormControl('', [Validators.required]);
+  nameFormControl = new FormControl('', [Validators.required]);
+  lastNameFormControl = new FormControl('', [Validators.required]);
+  cityFormControl = new FormControl('', [Validators.required]);
+  passwordFormControl = new FormControl('', [Validators.required, Validators.minLength(6)]);
+  password2FormControl = new FormControl('', [Validators.required]);
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
